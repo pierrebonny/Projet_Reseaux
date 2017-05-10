84daf66f646934ac1ce09a43c33b458a91bf6535
Thomas Tetu <thomas.tetu@etu.unice.fr>
2017-05-10 13:30:34 +0200

    IdeaManager

--- Binome_Merlet_Tetu/src/main/java/model/IdeaManager.java
+++ Binome_Merlet_Tetu/src/main/java/model/IdeaManager.java
@@ -25,4 +25,21 @@ public class IdeaManager {
     public Idea getIdea(int index){
         return this.ideas.get(index);
     }
+
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        s.append("---------------------------------\n");
+        for(int i =0;i<ideas.size();i++){
+            Idea idea = ideas.get(i);
+            s.append("Idée " + (char) i +" :\n");
+            s.append("Nom : " + idea.getName() +"\n");
+            s.append("Description : "+ idea.getDescription()+ "\n");
+            s.append("Technologie : " + idea.getTechnology()+"\n");
+            s.append("Créateur : \n\t"+ idea.getNomCreator() +"\n\t" );
+            s.append(idea.getMailCreator() +"\n");
+            s.append("Finalisé : " + idea.isProject()+"\n");
+            s.append("---------------------------------\n");
+        }
+        return s.toString();
+    }
 }
